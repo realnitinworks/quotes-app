67741f7eba8308e267ced95569b7302f017d52e5
realnitinworks <realnitinworks@gmail.com>
2019-08-19 10:31:11 +0530

    Add permission and authorization to the app.

--- quotes/models.py
+++ quotes/models.py
@@ -1,16 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
 
 class Quote(models.Model):
     text = models.TextField()
-    author = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)  # author of the quote
     source = models.URLField(max_length=250, blank=True)
     cover = models.URLField(max_length=250, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        blank=True, null=True
+    )
 
     def __str__(self):
-        return f"{self.text} by {self.author}"
+        return f"{self.text} by {self.author} (submitted by {self.user})"
 
     def get_absolute_url(self):
         return reverse('quote_detail', args=f"{self.id}")
--- quotes/views.py
+++ quotes/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Quote
@@ -15,19 +16,35 @@ class QuotesDetailView(DetailView):
     context_object_name = 'quote'
 
 
-class QuotesCreateView(CreateView):
+class QuotesCreateView(LoginRequiredMixin, CreateView):
     model = Quote
     template_name = 'quote_new.html'
     fields = ('text', 'author', 'cover', 'source')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 
-class QuotesUpdateView(UpdateView):
+class QuotesUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Quote
     template_name = 'quote_edit.html'
     fields = ('text', 'author', 'cover', 'source')
+    login_url = 'login'
+
+    def test_func(self):
+        quote = self.get_object()
+        return quote.user == self.request.user
 
 
-class QuotesDeleteView(DeleteView):
+class QuotesDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Quote
     template_name = 'quote_delete.html'
     success_url = reverse_lazy('home')
+    login_url = 'login'
+
+    def test_func(self):
+        quote = self.get_object()
+        return quote.user == self.request.user
+
