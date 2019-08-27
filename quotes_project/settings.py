7a37a73ebb9df36d20684dd503b909ae0e22c31d
realnitinworks <realnitinworks@gmail.com>
2019-08-27 22:30:31 +0530

    Add a missing comma in settings.py

--- quotes_project/settings.py
+++ quotes_project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     'django.contrib.humanize',
